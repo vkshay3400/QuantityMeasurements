e16f522da6f2977f035d8d1413b7a969a3b5cee7
vkshay3400 <akshayp2208@gmail.com>
2020-04-08 18:49:08 +0530

    Added: Welcome messgae

---
+++ src/main/java/com/bridglelabz/quantitymeasurement/MeasurementOfQuantity.java
@@ -0,0 +1,7 @@
+package com.bridglelabz.quantitymeasurement;
+
+public class MeasurementOfQuantity {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quantity Measurements");
+    }
+}
