f4a76229631343734210f8e25e60d94fbc7bcf13
vkshay3400 <akshayp2208@gmail.com>
2020-04-09 20:48:34 +0530

    Modified: Method for inch feet

--- src/main/java/com/bridglelabz/quantitymeasurement/MeasurementOfQuantity.java
+++ src/main/java/com/bridglelabz/quantitymeasurement/MeasurementOfQuantity.java
@@ -1,7 +1,20 @@
 package com.bridglelabz.quantitymeasurement;
 
+import com.bridglelabz.quantitymeasurement.conversions.TypesConversion;
+
 public class MeasurementOfQuantity {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Quantity Measurements");
+    private static double FIRST_VALUE;
+    private static double SECOND_VALUE;
+
+    public double getConversions(TypesConversion type, double value) {
+        setPreValues(type);
+        double operatedAfterValue = value * SECOND_VALUE;
+        return operatedAfterValue;
+    }
+
+    private void setPreValues(TypesConversion type) {
+        FIRST_VALUE = type.firstValue;
+        SECOND_VALUE = type.secondValue;
+
     }
 }
