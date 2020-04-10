19e7ba6f159401b034b1331d90552cb189c4e084
vkshay3400 <akshayp2208@gmail.com>
2020-04-10 15:42:46 +0530

    Added: Enum type for inch to centimeter

--- src/main/java/com/bridglelabz/quantitymeasurement/conversions/TypesConversion.java
+++ src/main/java/com/bridglelabz/quantitymeasurement/conversions/TypesConversion.java
@@ -3,7 +3,8 @@ package com.bridglelabz.quantitymeasurement.conversions;
 public enum TypesConversion {
     FEET_TO_INCH(1, 12), INCH_TO_FEET(1, (1 / 12)),
     FEET_YARD(1, 3), YARD_FEET(3, 1),
-    INCH_YARD(1, 36), YARD_INCH(36, 1);
+    INCH_YARD(1, 36), YARD_INCH(36, 1),
+    INCH_CENTIMETER(5,2 );
 
     public final double firstValue;
     public final double secondValue;
