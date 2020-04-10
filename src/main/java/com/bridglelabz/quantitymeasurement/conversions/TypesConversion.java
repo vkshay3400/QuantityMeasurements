b0ec1dc9f2826c277344093cac41655d5dcd19cb
vkshay3400 <akshayp2208@gmail.com>
2020-04-10 18:35:55 +0530

    Added: Type conversions for kilogram, grams, tonne

--- src/main/java/com/bridglelabz/quantitymeasurement/conversions/TypesConversion.java
+++ src/main/java/com/bridglelabz/quantitymeasurement/conversions/TypesConversion.java
@@ -1,12 +1,13 @@
 package com.bridglelabz.quantitymeasurement.conversions;
 
 public enum TypesConversion {
-    FEET_TO_INCH(12, 1), INCH_TO_FEET(1, (1/12.0)),
+    FEET_TO_INCH(12, 1), INCH_TO_FEET(1, (1 / 12.0)),
     FEET_YARD(1, 3), YARD_FEET(3, 1),
     INCH_YARD(1, 36), YARD_INCH(36, 1),
-    INCH_CENTIMETER(5,2 ),CENTIMETER_INCH(2,5 ),
-    GALLON_TO_LITRE(3.78,1 ),LITRE_TO_GALLON(1,3.78),
-    LITRE_TO_MILLILITRE(1000,1),MILLILITRE_TO_LITRE(1,1000);
+    INCH_CENTIMETER(5, 2), CENTIMETER_INCH(2, 5),
+    GALLON_TO_LITRE(3.78, 1), LITRE_TO_MILLILITRE(1000, 1),
+    KILOGRAMS_TO_GRAMS(1000, 1), GRAMS_TO_KILOGRAMS(1, 1000),
+    TONNE_TO_KILOGRAMS(1000, 1);
 
     public final double firstValue;
     public final double secondValue;
