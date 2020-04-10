d6b869cdab47848023654a00dae1b4ad2ad44ee2
vkshay3400 <akshayp2208@gmail.com>
2020-04-10 17:00:27 +0530

    Added: Centimeter to inches type conversion

--- src/main/java/com/bridglelabz/quantitymeasurement/conversions/TypesConversion.java
+++ src/main/java/com/bridglelabz/quantitymeasurement/conversions/TypesConversion.java
@@ -4,7 +4,7 @@ public enum TypesConversion {
     FEET_TO_INCH(12, 1), INCH_TO_FEET(1, (1/12.0)),
     FEET_YARD(1, 3), YARD_FEET(3, 1),
     INCH_YARD(1, 36), YARD_INCH(36, 1),
-    INCH_CENTIMETER(5,2 );
+    INCH_CENTIMETER(5,2 ),CENTIMETER_INCH(2,5 );
 
     public final double firstValue;
     public final double secondValue;
