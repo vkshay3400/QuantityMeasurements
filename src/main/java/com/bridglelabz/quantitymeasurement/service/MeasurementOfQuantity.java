4fe1318e1c3d15bed61e947d5f28a5d1ba293157
vkshay3400 <akshayp2208@gmail.com>
2020-04-10 15:44:08 +0530

    Modified: Method for inch to centimeter

--- src/main/java/com/bridglelabz/quantitymeasurement/service/MeasurementOfQuantity.java
+++ src/main/java/com/bridglelabz/quantitymeasurement/service/MeasurementOfQuantity.java
@@ -3,19 +3,16 @@ package com.bridglelabz.quantitymeasurement.service;
 import com.bridglelabz.quantitymeasurement.conversions.TypesConversion;
 
 public class MeasurementOfQuantity {
+    //STATIC VARIABLES
     private static double FIRST_VALUE;
     private static double SECOND_VALUE;
 
-    public double getConversions(TypesConversion type, double value) {
-        setPreValues(type);
-        double operatedAfterValue = value * SECOND_VALUE;
-        return operatedAfterValue;
-    }
-
-    public boolean getYardFeetConversions(TypesConversion type, double value1, double value2) {
+    //METHOD TO GET CONVERSIONS
+    public boolean getConversion(TypesConversion type, double value1, double value2) {
         setPreValues(type);
         double operation1 = value1 * FIRST_VALUE;
         double operation2 = value2 * SECOND_VALUE;
+
         if (operation1 == operation2) {
             return true;
         } else {
@@ -23,6 +20,7 @@ public class MeasurementOfQuantity {
         }
     }
 
+    //TO SET VALUES
     private void setPreValues(TypesConversion type) {
         FIRST_VALUE = type.firstValue;
         SECOND_VALUE = type.secondValue;
