4985e1d5d5a6b022c7d1210905294cb5c22cb57e
vkshay3400 <akshayp2208@gmail.com>
2020-04-12 11:04:37 +0530

    Modified: Variable name

--- src/main/java/com/bridglelabz/quantitymeasurement/service/MeasurementOfQuantity.java
+++ src/main/java/com/bridglelabz/quantitymeasurement/service/MeasurementOfQuantity.java
@@ -10,10 +10,11 @@ public class MeasurementOfQuantity {
     private static double SECOND_VALUE;
 
     //METHOD TO GET CONVERSIONS
-    public boolean getConversion(TypesConversion type, double value1, double value2) {
+
+    public boolean getConversion(TypesConversion type, double beforeConversionValue, double afterConversionValue) {
         setPreValues(type);
-        double operation1 = value1 * FIRST_VALUE;
-        double operation2 = value2 * SECOND_VALUE;
+        double operation1 = beforeConversionValue * FIRST_VALUE;
+        double operation2 = afterConversionValue * SECOND_VALUE;
 
         if (Objects.equals(operation1, operation2))
             return true;
